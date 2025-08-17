d721b7f14e04d24fcac069523deacdadb2967e14
cojiso <123448817+cojiso@users.noreply.github.com>
2025-08-17 21:45:50 +0900

    chore: コードをシンプルに

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,14 +1,10 @@
 import './style.css';
 import App from './App.svelte';
 
-const target = document.getElementById('app');
-
-if (!target) {
-  throw new Error('Could not find app element');
-}
+const target = document.getElementById('app') as Element;
 
 const app = new App({
-  target: target,
+  target,
 });
 
 export default app;
